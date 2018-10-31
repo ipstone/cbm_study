1b253c88a6bc1b79ea8660f4ac23938994a83699
Isaac Pei <peix@mskcc.org>
2018-10-31 14:26:40 -0400

    add theme to repo, and deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+echo 'Please have hugo installed: https://gohugo.io/'
+cp docs/README.md hugo/content/index.md
+
+# Clean the public folder before rebuild - not sure why needed but issues on some machine
+rm -rf  hugo/public/*
+
+# Generating the hugo site
+cd hugo
+hugo
+
+# Uploading generated site to 
+cd ../
+echo "Please use rsync -avz public/ server:www/cbmministry/wed_study/ to upload to server."
